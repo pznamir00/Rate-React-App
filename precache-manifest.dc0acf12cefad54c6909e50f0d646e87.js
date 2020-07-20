3fb73ccb986ee2c2663d0d3aa50de1ab2eca6e62
unknown <pznamir00@gmail.com>
2020-07-20 21:25:36 +0200

    Updates

---
+++ precache-manifest.dc0acf12cefad54c6909e50f0d646e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f855c19e8883622d05763512dd3c7e86",
+    "url": "/Rate-React-App/index.html"
+  },
+  {
+    "revision": "7465b7906e78c547ba16",
+    "url": "/Rate-React-App/static/css/main.a01919b5.chunk.css"
+  },
+  {
+    "revision": "a1188c1f840345da1e96",
+    "url": "/Rate-React-App/static/js/2.62c5a26a.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/Rate-React-App/static/js/2.62c5a26a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7465b7906e78c547ba16",
+    "url": "/Rate-React-App/static/js/main.f1b21383.chunk.js"
+  },
+  {
+    "revision": "2ca54c5eeed4fa46f1e5",
+    "url": "/Rate-React-App/static/js/runtime-main.6a54bbdf.js"
+  }
+]);
